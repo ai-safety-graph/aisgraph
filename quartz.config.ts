751ac00814da5415aff3cfc267b82e6c9e592a81
savva <savva.lambin@gmail.com>
2025-03-19 00:37:26 +0000

    Update Typography

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Montserrat",
+        body: "Open sans",
         code: "IBM Plex Mono",
       },
       colors: {
